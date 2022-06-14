192a21177d0460b649fa87abe231fc4c78bce649
meetsridhar9 <meetsridhar9@gmail.com>
2022-06-13 20:08:25 -0700

    Scopes and xml config chkin

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,18 +1,61 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import guru.springframework.pets.PetService;
+import guru.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import guru.springframework.sfgdi.services.*;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Service;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
     @Bean
-    ConstructorGreetingService constructorGreetingService(){
-        return new ConstructorGreetingService();
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
     }
+
+    @Profile({"dog","default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Bean
+    @Profile("cat")
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
+    }
+    @Primary
+    @Bean
+    PrimaryGreetingService PrimaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+   // @Bean
+ //   ConstructorGreetingService constructorGreetingService(){
+   //     return new ConstructorGreetingService();
+   // }
 
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService(){
